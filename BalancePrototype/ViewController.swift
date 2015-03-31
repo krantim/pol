1d705fcfe7ef23e0821f9a5754caa858c081977e
Chanintorn Asavavichairoj <aijojoe@gmail.com>
2015-03-31 15:40:23 +0700

    Initial Commit

---
+++ BalancePrototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BalancePrototype
+//
+//  Created by Chanintorn Asavavichairoj on 3/31/2558 BE.
+//  Copyright (c) 2558 SCB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
