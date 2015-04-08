8c9ea77aa7d8699a96a73b6b70e9d389391808ec
Chanintorn Asavavichairoj <aijojoe@gmail.com>
2015-04-08 14:51:17 +0700

    Initial Commit

---
+++ POL/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  POL
+//
+//  Created by Chanintorn Asavavichairoj on 4/8/2558 BE.
+//  Copyright (c) 2558 SCB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
