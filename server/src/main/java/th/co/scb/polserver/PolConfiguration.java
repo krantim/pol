fbe982fc21d7347f2d1a7f65f9ffd120f2126c06
Christian Trabold <ctrabold@thoughtworks.com>
2015-04-17 14:56:39 +0700

    [salah] setup a temporary vagrant vm to convert mobile-server jar to rpm

--- server/src/main/java/th/co/scb/polserver/PolConfiguration.java
+++ server/src/main/java/th/co/scb/polserver/PolConfiguration.java
@@ -15,9 +15,9 @@ public class PolConfiguration extends Configuration {
 
     @Valid
     @NotNull
+    @JsonProperty
     private HttpClientConfiguration httpClient = new HttpClientConfiguration();
 
-    @JsonProperty("httpClient")
     public HttpClientConfiguration getHttpClientConfiguration() {
         return httpClient;
     }
